24f465ca404742934f4814473a0db48a4466d28c
Pedro Tavolaro <phtc-pedro@hotmail.com>
2022-07-21 16:17:25 -0300

    test two committ

--- src/Router.tsx
+++ src/Router.tsx
@@ -8,15 +8,24 @@ export function Router(){
 
     return(
         <Routes>
-               {
-                emailLocal === null ?
-                <Route path="/" element={<Subscribe />} />
-                :
+            
+                {emailLocal === null ? 
                 <>
+                <Route path="/" element={<Subscribe />} />
                 <Route path="/event" element={<Event />} />
                 <Route path="/event/lesson/:slug" element={<Event />} />
                 </>
-               } 
+                :
+                <>
+                <Route path="/" element={<Event />} />
+                <Route path="/event" element={<Event />} />
+                <Route path="/event/lesson/:slug" element={<Event />} />
+                </>
+                }
+                
+                
+                
+               
             
            
         </Routes>
